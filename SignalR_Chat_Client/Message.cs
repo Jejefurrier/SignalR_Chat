459c2429c05f01247760643eaa5ce40ca0296f78
Jeferson <jeferson_51@hotmail.com>
2022-03-05 14:11:22 -0300

    Created a functional client

---
+++ SignalR_Chat_Client/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR_Chat_Client
+{
+    public class Message
+    {
+        public string ChatName { get; set; }
+        public string Username { get; set; }
+        public string TextMessage { get; set; }
+    }
+}
---
+++ SignalR_Chat_Client/Program.cs
@@ -0,0 +1,56 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Threading.Channels;
+using Microsoft.AspNetCore.SignalR.Client;
+using SignalR_Chat_Client;
+
+while (true)
+{
+    Console.WriteLine("Welcome to SignalR_Chat, please tell me your nickname!");
+    var userName = Console.ReadLine();
+
+    Console.WriteLine("Please tell me the room you want to join!");
+    var chatName = Console.ReadLine();
+    Console.WriteLine("If you want to leave this room, type /quit");
+    HubConnection connection;
+       connection = new HubConnectionBuilder()
+        .WithUrl("https://localhost:44320/chat")
+        .Build();
+
+    connection.Closed += async (error) =>
+    {
+        await Task.Delay(new Random().Next(0, 5) * 1000);
+        await connection.StartAsync();
+    };
+
+    connection.On<Message>("MessageReceived", (message) =>
+    {
+        Console.WriteLine(message.Username + ": " + message.TextMessage);
+    });
+
+    await connection.StartAsync();
+
+    await connection.InvokeAsync("JoinChat", chatName);
+
+
+
+    while (true)
+    {
+        var message = Console.ReadLine();
+        if (message == "/quit")
+        {
+            await connection.InvokeAsync("LeaveChat", chatName);
+            Console.WriteLine("You left " + chatName);
+            break;
+        }
+        else
+        {
+            await connection.InvokeAsync("SendMessage", new Message()
+            {
+                ChatName = chatName,
+                TextMessage = message,
+                Username = userName
+            });
+        }
+    };
+}
